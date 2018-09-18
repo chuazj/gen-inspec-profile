a674736514142f568b7d4418ea06cb5f68cf9422
Chuazj <zijian@hotmail.sg>
2018-09-18 15:49:20 +0800

    Update example.rb

--- controls/example.rb
+++ controls/example.rb
@@ -2,5 +2,5 @@
 # copyright: 2018, The Authors
 
 describe http('http://10.88.104.21/reports/', enable_remote_worker: true) do
-  its('status') { should cmp 200 }
+  its('status') { should cmp 200 
 end
