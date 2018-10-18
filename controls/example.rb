ea61ab1fd486519037a20a546970adfd5b02b707
Chuazj <zijian@hotmail.sg>
2018-10-18 11:26:23 +0800

    Update example.rb

--- controls/example.rb
+++ controls/example.rb
@@ -12,3 +12,15 @@ end
 describe http('http://10.88.104.21/reports/', enable_remote_worker: true) do
   its('status') { should cmp 200 }
 end
+
+describe http('http://10.88.104.21/reports/', enable_remote_worker: true) do
+  its('status') { should cmp 200 }
+end
+
+describe http('http://10.88.104.21/reports/', enable_remote_worker: true) do
+  its('status') { should cmp 200 }
+end
+
+describe http('http://10.88.104.21/reports/', enable_remote_worker: true) do
+  its('status') { should cmp 200 }
+end
